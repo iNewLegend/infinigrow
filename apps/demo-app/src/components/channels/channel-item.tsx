e989616f8552ae1c82b10b02b0f62cd293088287
Leonid Vinikov <czf.leo123@gmail.com>
2023-12-08 08:11:32 +0200

    New (`demo-app`) - Channel budget settings, `manually` breakdown
    Moment library was added to package.json for handling dates. "ChannelItem" component now includes a section for budget breakdown and uses the 'moment' library to display monthly budget distributions.
    These changes are required to show a detailed breakdown of the budget allocation over different time periods and improve the user interface.

--- apps/demo-app/src/components/channels/channel-item.tsx
+++ apps/demo-app/src/components/channels/channel-item.tsx
@@ -2,6 +2,14 @@ import React from "react";
 
 import { withCommands } from "@infinigrow/commander/with-commands";
 
+import { CommandBase } from "@infinigrow/commander/command-base";
+
+import { Input } from "@nextui-org/input";
+
+import moment from "moment";
+
+import { useComponentCommands } from "@infinigrow/commander/use-commands";
+
 import {
     ChannelBudgetFrequency,
     ChannelBudgetBaseline,
@@ -16,7 +24,91 @@ import type {
 import type { ChannelItemProps } from "@infinigrow/demo-app/src/components/channels/channel-types";
 
 import type { CommandFunctionComponent, CommandArgs } from "@infinigrow/commander/types";
-import { CommandBase } from "@infinigrow/commander/command-base";
+import type { InputProps } from "@nextui-org/input";
+
+const DEFAULT_INPUT_PROPS: InputProps = {
+    classNames: {
+        base: "input",
+        mainWrapper: "wrapper",
+        inputWrapper: "trigger",
+        label: "currency",
+    },
+    type: "string",
+    variant: "bordered",
+    radius: "none",
+    labelPlacement: "outside",
+    placeholder: "0",
+    startContent: (
+        <div className="pointer-events-none flex items-center">
+            <span className="text-slate-700 text-sm font-medium leading-[21px]">$</span>
+        </div>
+    ),
+};
+
+function getBreaks( frequency: ChannelBudgetFrequencyProps["frequency"], baseline: string, allocation: BudgetAllocationType ) {
+    const breaks: React.JSX.Element[] = [];
+
+    const Break: React.FC<{ label: string; value: string }> = ( props ) => {
+        const { label, value } = props;
+
+        const formatted = ( parseFloat( value ) ).toLocaleString(
+            undefined,
+            {
+                minimumFractionDigits: 0,
+                maximumFractionDigits: 2,
+            }
+        );
+
+        return (
+            <div className="break">
+                <Input { ... DEFAULT_INPUT_PROPS } label={ label } value={ formatted }></Input>
+            </div>
+        );
+    };
+
+    const baselineParsed = parseFloat( baseline.replace( /,/g, "" ) );
+
+    switch ( frequency ) {
+        case "annually":
+            const perMonth = baselineParsed / 12;
+
+            for ( let i = 0 ; i < 12 ; i++ ) {
+                const month = moment().month( i ).format( "MMM D" );
+
+                breaks.push( (
+                    <Break key={ i } label={ month } value={ perMonth.toString() }/> ) );
+            }
+            break;
+    }
+
+    return breaks;
+}
+
+export const ChannelBreakdowns: React.FC<{
+    frequency: ChannelBudgetFrequencyProps["frequency"];
+    baseline: string;
+    allocation: BudgetAllocationType;
+}> = ( props ) => {
+    const { frequency, baseline, allocation } = props;
+
+    const commands = useComponentCommands( "App/ChannelItem" );
+
+    const [ breaks, setBreaks ] = React.useState<React.JSX.Element[]>(
+        getBreaks( frequency, baseline, allocation )
+    );
+
+    React.useEffect( () => {
+        commands.hook( "App/ChannelItem/SetBaseline", ( args ) => {
+            setBreaks( getBreaks( frequency, args.value, allocation ) );
+        } );
+    }, [] );
+
+    return (
+        <div className="content">
+            { breaks }
+        </div>
+    );
+};
 
 export const ChannelItem: CommandFunctionComponent<ChannelItemProps> = ( props ) => {
     const [ frequency, setFrequency ] =
@@ -33,6 +125,15 @@ export const ChannelItem: CommandFunctionComponent<ChannelItemProps> = ( props )
                 <ChannelBudgetBaseline frequency={ frequency } baseline={ baseline } setBaseline={ setBaseline }/>
                 <ChannelBudgetAllocation allocation={ allocation } setAllocation={ setAllocation }/>
             </div>
+
+            <div className="channel-budget-breakdowns">
+                <div className="header">
+                    <p className="fs-2">Budget Breakdown</p>
+                    <p className="description">By default, your budget will be equally divided throughout the year. You
+                        can manually change the budget allocation, either now or later.</p>
+                    <ChannelBreakdowns frequency={ frequency } baseline={ baseline } allocation={ allocation }/>
+                </div>
+            </div>
         </div>
     );
 };
@@ -42,6 +143,7 @@ const $$ = withCommands( "App/ChannelItem", ChannelItem, [
         public static getName() {
             return "App/ChannelItem/SetAllocation";
         }
+
         protected apply( args: CommandArgs ) {
             const { value, setAllocation } = args;
 
@@ -54,6 +156,7 @@ const $$ = withCommands( "App/ChannelItem", ChannelItem, [
         public static getName() {
             return "App/ChannelItem/SetBaseline";
         }
+
         protected apply( args: CommandArgs ) {
             let result;
 
@@ -62,7 +165,8 @@ const $$ = withCommands( "App/ChannelItem", ChannelItem, [
             // If include alphabet, then halt
             if ( /[a-zA-Z]/.test( value ) ) {
                 return;
-            };
+            }
+            ;
 
             // Remove leading zeros.
             result = value.replace( /^0+/, "" );
@@ -85,6 +189,7 @@ const $$ = withCommands( "App/ChannelItem", ChannelItem, [
         public static getName() {
             return "App/ChannelItem/SetFrequency";
         }
+
         protected apply( args: CommandArgs ) {
             const { value, setFrequency } = args;
 
